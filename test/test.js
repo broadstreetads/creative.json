1367d6aa2bffc590b14165ef941f5178cbc6dd96
Kenny Katzgrau <katzgrau@gmail.com>
2018-02-03 21:36:19 -0500

    Add simple compilation

--- test/test.js
+++ test/test.js
@@ -6,12 +6,22 @@ const fs = require('fs')
 
 const runner = new TestRunner()
 
-let impl = fs.readFileSync('samples/creative-impl.json', 'utf-8')
-impl = JSON.parse(impl)
-
 runner.test('validate example creative', function (t) {
+    let impl = fs.readFileSync('samples/creative-impl.json', 'utf-8')
+    impl = JSON.parse(impl)
     a.strictEqual(
         creative.validate(impl),
         true
+    )
+})
+
+runner.test('compile simple creative', function (t) {
+    let impl = fs.readFileSync('samples/simple.json', 'utf-8')
+    impl = JSON.parse(impl)
+    a.strictEqual(
+        creative.compile(impl, {
+            name: 'Broadstreet Ads'
+        }),
+        '<style>/* CSS: Broadstreet Ads */</style><div><!-- HTML:Broadstreet Ads --></div><script>// JS:Broadstreet Ads</script>'
     )
 })
